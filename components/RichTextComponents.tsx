d191e9338a1b6ed35e9ec2e98ba944fc9037e3ad
DanArcherOnline <danarcheronline@gmail.com>
2023-06-26 00:17:34 +0900

    ✨add inline code support

--- components/RichTextComponents.tsx
+++ components/RichTextComponents.tsx
@@ -48,17 +48,24 @@ export const RichTextComponents = {
                 </span>
             )
         },
+        code: ({ children }: any) => {
+            return (
+                <span className="font-mono text-base bg-[#1e1e1e] px-2 py-1 mx-1 rounded-lg border-[1px] border-body-text-grey">
+                    {children}
+                </span>
+            )
+        },
 
     },
     block: {
-        normal: ({ children }: any) => <p className="font-sourceSerif4 pb-4 text-xl">{children}</p>,
+        normal: ({ children }: any) => <p className="font-sourceSerif4 pb-4 text-xl leading-9">{children}</p>,
         h1: ({ children }: any) => <h1 className="font-inter pb-4 pt-10 text-6xl">{children}</h1>,
         h2: ({ children }: any) => <h2 className="font-inter pb-4 pt-8 text-5xl">{children}</h2>,
         h3: ({ children }: any) => <h3 className="font-inter pb-2 pt-4 text-4xl">{children}</h3>,
         h4: ({ children }: any) => <h4 className="font-inter text-3xl pb-2 pt-4">{children}</h4>,
         h5: ({ children }: any) => <h5 className="font-inter text-2xl pb-2 pt-2">{children}</h5>,
         h6: ({ children }: any) => <h6 className="font-inter text-xl pb-2 pt-2">{children}</h6>,
-        blockquote: ({ children }: any) => <blockquote className="border-l-accent-pink border-l-2 pl-4 mt-4 mb-10 italic font-sourceSerif4 text-xl">{children}</blockquote>,
+        blockquote: ({ children }: any) => <blockquote className="border-l-accent-pink border-l-2 pl-4 mt-4 mb-10 italic font-sourceSerif4 text-xl leading-9">{children}</blockquote>,
 
     },
     list: {
