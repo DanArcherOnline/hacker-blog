aa5275781e8a81ed9e67a14e833d5f2a9f8bb439
DanArcherOnline <danarcheronline@gmail.com>
2023-07-12 20:31:41 +0900

    🐛replace static dummy data with dynamic data

--- components/Certificates.tsx
+++ components/Certificates.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Certificates({ certificates }: Props) {
     return (
         <div className="space-y-4 ">
-            <h1 className="font-black text-xl ml-2">Certificates (3)</h1>
+            <h1 className="font-black text-xl ml-2">Certificates ({certificates.length})</h1>
             {certificates.map((certificate, i) => <CertificateItem key={i} certificate={certificate} />)}
         </div>
     )
