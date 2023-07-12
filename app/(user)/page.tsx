44f97324e8e1bb36f1393beaed49c438f1483c90
DanArcherOnline <danarcheronline@gmail.com>
2023-07-12 20:42:51 +0900

    ✨add revalidation to homepage

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,6 +1,8 @@
 import Homepage from "@/components/Homepage";
 import { getCertificates, getCtfBadges, getFeaturedPost, getPosts } from "@/sanity/sanity-utils";
 
+export const revalidate = 60
+
 export default async function Home() {
   const posts = await getPosts()
   const featuredPost = await getFeaturedPost()
